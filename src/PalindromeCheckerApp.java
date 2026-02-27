ad0c5b921a76f9355730d827d6da534b5c1504a7
itsrain1006 <myraanand06@gmail.com>
2026-02-27 15:51:22 +0530

    UC2:Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,30 +1,17 @@
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Deque;
-import java.util.ArrayDeque;
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+        String hardcoded = "madam";
+        String reverseHardcoded = "";
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("\nEnter a string:");
-        String input1 = scanner.nextLine();
-
-        String reverse1 = "";
-
-        for (int i = input1.length() - 1; i >= 0; i--) {
-            reverse1 += input1.charAt(i);
+        for (int i = hardcoded.length() - 1; i >= 0; i--) {
+            reverseHardcoded += hardcoded.charAt(i);
         }
 
-        if (input1.equals(reverse1)) {
-            System.out.println(input1 + " is a Palindrome (UC3)");
+        if (hardcoded.equals(reverseHardcoded)) {
+            System.out.println(hardcoded + " is a Palindrome");
         } else {
-            System.out.println(input1 + " is Not a Palindrome (UC3)");
+            System.out.println(hardcoded + " is Not a Palindrome");
         }
-        scanner.close();
     }
 }
