db192a473622e8704ab2ce405aea1ca20e680bd2
itsrain1006 <myraanand06@gmail.com>
2026-02-27 15:07:24 +0530

    UC5:Stack based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -10,30 +10,27 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\nEnter a string for UC4:");
-        String input2 = scanner.nextLine();
+        System.out.println("\nEnter a string for UC5:");
+        String input3 = scanner.nextLine();
 
-        char[] chars = input2.toCharArray();
+        Stack<Character> stack1 = new Stack<>();
 
-        int start = 0;
-        int end = chars.length - 1;
-
-        boolean isPalindrome = true;
-
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+        for (int i = 0; i < input3.length(); i++) {
+            stack1.push(input3.charAt(i));
         }
 
-        if (isPalindrome) {
-            System.out.println(input2 + " is a Palindrome (UC4)");
+        String reversed = "";
+
+        while (!stack1.isEmpty()) {
+            reversed += stack1.pop();
+        }
+
+        if (input3.equals(reversed)) {
+            System.out.println(input3 + " is a Palindrome (UC5)");
         } else {
-            System.out.println(input2 + " is Not a Palindrome (UC4)");
+            System.out.println(input3 + " is Not a Palindrome (UC5)");
         }
+
         scanner.close();
     }
 }
